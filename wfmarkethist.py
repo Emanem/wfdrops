3e2fd4a8991556b80a056f227ca877c41320990b
Emanuele Oriani <Emanem@github.com>
2022-06-11 18:44:06 +0100

    wfmarkethist: initial stab at adding tags of items - suboptimal

--- wfmarkethist.py
+++ wfmarkethist.py
@@ -21,6 +21,8 @@ import math
 G_DB_NAME = "wf_mkt_hist.db"
 G_DB_ITEMS_NAME = "items"
 G_DB_ITEMS_HIST = "hist"
+G_DB_TAGS_NAME = "tags"
+G_DB_ITEMS_TAGS = "items_attrs"
 G_WFM_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 G_SLEEP_THROTTLE = 0.5
 
@@ -29,17 +31,19 @@ def db_setup(db):
     cur.execute("CREATE TABLE IF NOT EXISTS " + G_DB_ITEMS_NAME + " (name text)")
     cur.execute("CREATE TABLE IF NOT EXISTS " + G_DB_ITEMS_HIST + " (id integer, ts timestamp, volume integer, min integer, max integer, open integer, close integer, avg real, w_avg real, median real, m_avg real)")
     cur.execute("CREATE INDEX IF NOT EXISTS i1 ON " + G_DB_ITEMS_HIST + "(id)")
+    cur.execute("CREATE TABLE IF NOT EXISTS " + G_DB_TAGS_NAME + " (name text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS " + G_DB_ITEMS_TAGS + " (item_id integer, tag_id integer)")
     db.commit()
     return None
 
-def db_fetch_names(db, nm):
+def db_fetch_names(db, tb_nm, nm):
     cur = db.cursor()
     for i in nm:
-        q = "INSERT INTO " + G_DB_ITEMS_NAME + "(name) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM " + G_DB_ITEMS_NAME + " WHERE name=?)";
+        q = "INSERT INTO " + tb_nm + "(name) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM " + tb_nm + " WHERE name=?)";
         cur.execute(q, (i, i))
     db.commit()
     rv = {}
-    ri = cur.execute("SELECT MAX(rowid) as id, name FROM " + G_DB_ITEMS_NAME + " WHERE 1=1 GROUP BY name")
+    ri = cur.execute("SELECT MAX(rowid) as id, name FROM " + tb_nm + " WHERE 1=1 GROUP BY name")
     for i in ri:
         rv[i[1]] = i[0]
     return rv
@@ -53,18 +57,25 @@ def db_fetch_ts(db, nm_id):
     return rv
 
 def db_insert_raw_data(db, all_data):
-    nm_id = db_fetch_names(db, all_data.keys())
+    nm_id = db_fetch_names(db, G_DB_ITEMS_NAME, all_data.keys())
     cur = db.cursor()
     rv_stats = {}
     for k, v in all_data.items():
         rv_stats[k] = 0
         # select all dates for given item
+        # and insert missing dates
         all_ts = db_fetch_ts(db, nm_id[k])
-        for r in v:
+        for r in v[0]:
             if r[0] in all_ts.keys():
                 continue
             cur.execute("INSERT INTO " + G_DB_ITEMS_HIST + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (nm_id[k], r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9]))
             rv_stats[k] = rv_stats[k]+1
+        # push all the tags - we're fetching more than
+        # required but it's ok for now
+        cur_tags = db_fetch_names(db, G_DB_TAGS_NAME, v[1])
+        # add the tags for a given item
+        for r in v[1]:
+            cur.execute("INSERT INTO " + G_DB_ITEMS_TAGS + "(item_id, tag_id) SELECT ?, ? WHERE NOT EXISTS (SELECT 1 FROM " + G_DB_ITEMS_TAGS + " WHERE item_id=? and tag_id=?)", (nm_id[k], cur_tags[r], nm_id[k], cur_tags[r]))
     db.commit()
     return rv_stats
 
@@ -86,14 +97,25 @@ def parse_hist_stats(data):
             rv.append((datetime.datetime.fromisoformat(x['datetime']), int(x['volume']), int(x['min_price']), int(x['max_price']), int(x['open_price']), int(x['closed_price']), float(x['avg_price']), float(x['wa_price']), float(x['median']), float(x.get('moving_avg', 0.0))))
     return rv
 
+def parse_attrs(data):
+    jdata = json.loads(data)
+    rv = {}
+    for x in jdata['payload']['item']['items_in_set']:
+        for y in x['tags']:
+            rv[y] = 1
+    return list(rv.keys())
+
 def get_wfm_webapi(str_url, https_cp):
     f = https_cp.urlopen('GET', str_url, headers={'User-Agent': G_WFM_USER_AGENT})
     return f.data.decode('utf-8')
 
-def get_hist_stats(item_name, https_cp):
+def get_hist_stats(item_name, https_cp, https_cp_api):
     str_url = '/items/' + item_name + '/statistics'
     data = get_wfm_webapi(str_url, https_cp)
-    return parse_hist_stats(data)
+    time.sleep(G_SLEEP_THROTTLE)
+    str_url = '/v1/items/' + item_name
+    data_attrs = get_wfm_webapi(str_url, https_cp_api)
+    return (parse_hist_stats(data), parse_attrs(data_attrs))
 
 def store_hist_data(item_names):
     print("\tFetching:")
@@ -102,19 +124,20 @@ def store_hist_data(item_names):
     cnt = 0
     # create the HTTPS pool here
     https_cp = urllib3.HTTPSConnectionPool('warframe.market')
+    https_cp_api = urllib3.HTTPSConnectionPool('api.warframe.market')
     for nm, q_nm in item_names.items():
         cnt += 1
         print("[{count:{fill}{align}{width}}/{total}]".format(count=cnt, total=len(item_names), fill=' ', align='>', width=n_digits), end='\t')
         print(nm, end='...')
         tm_start = time.monotonic()
         try:
-            all_items[nm] = get_hist_stats(q_nm, https_cp)
+            all_items[nm] = get_hist_stats(q_nm, https_cp, https_cp_api)
         except Exception as e:
             print("Error, carrying on (", e, ")")
         else:
             # this is not great - but it does work...
             tm_end = time.monotonic()
-            print('done', tm_end-tm_start, 's', "(" + str(len(all_items[nm])) + " entries)")
+            print('done', tm_end-tm_start, 's', "(" + str(len(all_items[nm][0])) + " entries)")
         finally:
             tm_end = time.monotonic()
             sleep_throttle = G_SLEEP_THROTTLE - (tm_end - tm_start)
